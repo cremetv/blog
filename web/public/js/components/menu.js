0e5340af29a7a2f3d26144d95c0147803e96c4e1
Marcel Hauser <maha93@gmx.de>
2019-08-30 12:37:25 +0200

    fixed mobile menu

--- web/public/js/components/menu.js
+++ web/public/js/components/menu.js
@@ -1,5 +1,6 @@
 let menuOpen = false;
 
+const html = document.querySelector('html');
 const burger = document.querySelectorAll('.burger')[0];
 const menu = document.querySelectorAll('.main-menu')[0];
 
@@ -7,6 +8,7 @@ const openMenu = () => {
   menuOpen = true;
   burger.classList.add('open');
   menu.classList.add('open');
+  html.classList.add('overflow');
   TweenMax.to(menu, 1, {
     x: 0,
     ease: Power4.easeOut
@@ -17,6 +19,7 @@ const closeMenu = () => {
   menuOpen = false;
   burger.classList.remove('open');
   menu.classList.remove('open');
+  html.classList.remove('overflow');
   TweenMax.to(menu, 1, {
     x: '100vw',
     ease: Power4.easeOut
