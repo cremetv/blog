48d99ddd4aa671ec6bc49d4add206635bbd5d7a9
Marcel Hauser <maha93@gmx.de>
2019-08-30 16:56:11 +0200

    fixed categories in post list

--- web/public/js/main.js
+++ web/public/js/main.js
@@ -12,8 +12,31 @@
 window.onload = () => {
 
   console.log('sw!!');
+  let swRegistration;
 
-  if ('serviceWorker' in navigator) {
-    navigator.serviceWorker.register('./sw.js');
+  if ('serviceWorker' in navigator && 'PushManager' in window) {
+    navigator.serviceWorker.register('./sw.js')
+    .then(swReg => {
+      console.log('Service Worker is registered', swReg);
+
+      swRegistration = swReg;
+    })
+    .catch(err => {
+      console.error('Service Worker Error', err);
+    });
   }
+
+  self.addEventListener('push', function(event) {
+    console.log('[Service Worker] Push Received.');
+    console.log(`[Service Worker] Push had this data: "${event.data.text()}"`);
+
+    const title = 'Push Codelab';
+    const options = {
+      body: 'Yay it works.',
+      icon: 'images/icon.png',
+      badge: 'images/badge.png'
+    };
+
+    event.waitUntil(self.registration.showNotification(title, options));
+  });
 }
