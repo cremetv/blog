fd6b265ffdb2b825a52d18ccbc89541670d01c48
Marcel Hauser <maha93@gmx.de>
2019-09-30 15:59:15 +0200

    fixes

--- web/public/js/main.js
+++ web/public/js/main.js
@@ -18,17 +18,22 @@ window.onload = () => {
   console.log('sw!!');
   let swRegistration;
 
-  if ('serviceWorker' in navigator && 'PushManager' in window) {
-    navigator.serviceWorker.register('./sw.js')
-    .then(swReg => {
-      console.log('Service Worker is registered', swReg);
+  navigator.serviceWorker.getRegistrations().then(function(registrations) {
+   for(let registration of registrations) {
+    registration.unregister()
+  } })
 
-      swRegistration = swReg;
-    })
-    .catch(err => {
-      console.error('Service Worker Error', err);
-    });
-  }
+  // if ('serviceWorker' in navigator && 'PushManager' in window) {
+  //   navigator.serviceWorker.register('./sw.js')
+  //   .then(swReg => {
+  //     console.log('Service Worker is registered', swReg);
+  //
+  //     swRegistration = swReg;
+  //   })
+  //   .catch(err => {
+  //     console.error('Service Worker Error', err);
+  //   });
+  // }
 
   // self.addEventListener('push', function(event) {
   //   console.log('[Service Worker] Push Received.');
